d6f1f55e113edfcae6f18d28edc596a6b85461e7
HienPham99 <qctest898@gmail.com>
2023-04-12 00:38:52 +0700

    Push bai 5

---
+++ src/day5/AbstractUser.java
@@ -0,0 +1,12 @@
+package day5;
+
+public abstract class AbstractUser {
+	//Attribute
+	public String hoTen;
+	public String ngaySinh;
+	
+	//Class abstract chưa hàm abstract hoặc ko
+	abstract void inThongtin() ;
+	
+
+}
